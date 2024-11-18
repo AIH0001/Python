9ce89add15ba7d35959e229c308005df62ac2eca
Ariful Islam Himel <arifulislamhimel001@gmail.com>
2024-11-18 23:05:56 +0600

    Add files via upload

---
+++ output_operations_assignments.py
@@ -0,0 +1,21 @@
+"""
+print('Hello World')
+print('welcome to Python Programing. ')
+Name = 'John'
+Age= 25
+print(f'My name is{Name}, and I am {Age} years old.')
+
+Name = 'Jone'
+Age = 25
+print("My name is{}, and I am {} years old.".format(Name, Age))
+
+
+print('Name: Alice')
+print('Age: 30')
+print('Country: USA')
+
+
+
+print("Name: Alice\nAge: 30\nCountry: USA")
+
+"""

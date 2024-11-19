10c3571ead858e5ea3e066019ecbd226a0fb6260
Ariful Islam Himel <arifulislamhimel001@gmail.com>
2024-11-20 01:42:40 +0600

    Add files via upload

---
+++ assignment_2.py
@@ -0,0 +1,33 @@
+
+int1 = 10
+int2 = 5
+float1 = 3.5
+float2 = 2.5
+
+
+print('integer addition:', int1 + int2)
+print('integer subtraction:', int1 - int2)
+print('integer multiplication:', int1 * int2)
+print('integer division:', int1 / int2)
+
+
+print('float addition:', float1 + float2)
+print('float subtraction:', float1 + float2)
+print('float multiplication:', float1 + float2)
+print('float division:', float1 + float2)
+
+num1 = 20
+num2 = 3
+
+quotient = num1 // num2
+remainder = num1 % num2
+
+print('\nquotient:', quotient)
+print('remainder:', remainder) 
+
+str_num = '7'
+int_num = int(str_num)
+square = int_num * int_num
+
+print("\nSquare:", square)
+

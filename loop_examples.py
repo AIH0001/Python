9c0b75329dfed122c320f39ff3ca3615006d74b5
Ariful Islam Himel <arifulislamhimel001@gmail.com>
2025-12-11 23:36:46 +0600

    Add loop examples using list, tuple, and string

---
+++ loop_examples.py
@@ -0,0 +1,14 @@
+## For Loop with list
+l = [1,'himel', 'kader', True, 'moni', 'joy', 'akash']
+for i in l:
+    print(i)
+
+## For loop with tuple
+t = (12, 34, 56, 78, 90)
+for i in t:
+    print(i)
+
+## For loop with string
+s = "Himel's World"
+for i in s:
+    print(i)
